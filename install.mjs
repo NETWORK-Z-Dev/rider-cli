02550bd201ed4c353ceef83e62bbabf470fd8249
Marcel <40896559+hackthedev@users.noreply.github.com>
2026-08-18 19:45:57 +0200

    .

--- install.mjs
+++ install.mjs
@@ -26,8 +26,8 @@ fs.mkdirSync(installDir, {
     recursive: true
 });
 
-// download some shit
-const targetPath = path.join(installDir, binary);
+// download some shit and create symlink shit
+const targetPath = "/usr/local/bin/rider";
 const response = await fetch(`${baseUrl}/${binary}`);
 
 if (!response.ok) {
@@ -37,48 +37,6 @@ if (!response.ok) {
 const buffer = Buffer.from(await response.arrayBuffer());
 fs.writeFileSync(targetPath, buffer);
 fs.chmodSync(targetPath, 0o755);
-
-// create sym link so its nicer to use n shit, well prepair it actually
-const bunDir = path.dirname(process.execPath);
-let riderLink;
-
-// only relevant for linux. this shit is annoying me so much
-if(process.platform === "linux"){
-    const binDir = path.join(os.homedir(), ".local", "bin");
-    riderLink = path.join(binDir, "rider");
-
-    fs.mkdirSync(binDir, {
-        recursive: true
-    });
-
-    const shell = process.env.SHELL || "";
-    const shellConfig = shell.includes("zsh")
-        ? path.join(os.homedir(), ".zshrc")
-        : path.join(os.homedir(), ".bashrc");
-
-    const pathLine = `export PATH="$HOME/.local/bin:$PATH"`;
-
-    let shellConfigContent = fs.existsSync(shellConfig)
-        ? fs.readFileSync(shellConfig, "utf8")
-        : "";
-
-    if(!shellConfigContent.includes(pathLine)){
-        fs.appendFileSync(shellConfig, `\n${pathLine}\n`);
-    }
-
-    process.env.PATH = `${binDir}:${process.env.PATH}`;
-}
-else{
-    riderLink = path.join(bunDir, "rider");
-}
-
-// preay
-try {
-    fs.unlinkSync(riderLink);
-} catch {}
-
-fs.symlinkSync(targetPath, riderLink);
-
 
 
 console.log("Rider CLI installed");
